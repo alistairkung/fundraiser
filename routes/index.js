fe532bc10e8c76b4899571a542c340b5c2b2d1bc
terminalobject <doctorboredom@hotmail.com>
2017-10-16 16:15:33 +0100

    first working thing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var stripe = require('stripe')(
+  "sk_test_9gXbCG9wAEJ3HOFH5nvAFhJ0"
+);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var stripe = require('stripe')(
+    "sk_test_9gXbCG9wAEJ3HOFH5nvAFhJ0"
+  );
+  stripe.balance.retrieve(function(err, balance) {
+    var bal = balance;
+    res.render('index', { title: 'Express', balance: bal.available[0].amount});
+    // console.log(bal.available[0].amount);
+  });
+  //console.log(bal);
 });
-
+//console.log("log");
 module.exports = router;
